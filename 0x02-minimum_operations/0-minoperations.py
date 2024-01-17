22b93a569466ef0f314d04bf2f3a5bf89bfa4d43
Ahmedkel <ahmedkel@gmail.com>
2024-01-17 14:55:01 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""function file."""
+
+
+def minOperations(n):
+    """
+    calculates the fewest number of
+    operations needed to result in
+    """
+    if n <= 1:
+        return 0
+
+    min_operations = 0
+    current_length = 1
+    clipboard = 0
+
+    while current_length != n:
+        if n % current_length == 0:
+            clipboard = current_length
+            min_operations += 1
+        current_length += clipboard
+        min_operations += 1
+
+    return min_operations
