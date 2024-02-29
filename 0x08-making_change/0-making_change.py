b7ad5b565771b50f9ebc08a2d98bf32d3d7d3907
Ahmedkel <ahmedkeloch@gmail.com>
2024-02-29 16:18:49 +0100

    Add makeChange function to calculate change for a given amount of money

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+Module to make change for
+a given amount of money .
+"""
+
+
+def makeChange(coins, total):
+    """
+    Given a list of coins
+    and a total amount of money
+    """
+    if total <= 0:
+        return 0
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
