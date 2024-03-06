c4f4fd4fa501c7da497eeb19868a13dfafaf3ab6
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-06 15:54:34 +0100

    Refactor island_perimeter function

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
+""""""
 def island_perimeter(grid):
     """ Returns the perimeter of the island described by grid """
     ROWS, COLS = len(grid), len(grid[0])
     visited = set()
 
     def dfs(r, c):
-        # Base case: if out of bounds or on water,
-        # return 1 (edge touching water)
+        """ Returns the perimeter of the island described by grid """
         if r < 0 or r == ROWS or c < 0 or c == COLS or grid[r][c] == 0:
             return 1
         if (r, c) in visited:
@@ -15,7 +14,6 @@ def island_perimeter(grid):
         visited.add((r, c))
         return dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)
 
-    # Start from the land portion
     for r in range(ROWS):
         for c in range(COLS):
             if grid[r][c] == 1:
