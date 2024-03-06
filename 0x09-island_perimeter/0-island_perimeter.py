792916cc8a39bd81e161dda4b145ba8b9504f6fa
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-06 15:52:30 +0100

    Add island_perimeter function to calculate the perimeter of an island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """ Returns the perimeter of the island described by grid """
+    ROWS, COLS = len(grid), len(grid[0])
+    visited = set()
+
+    def dfs(r, c):
+        # Base case: if out of bounds or on water,
+        # return 1 (edge touching water)
+        if r < 0 or r == ROWS or c < 0 or c == COLS or grid[r][c] == 0:
+            return 1
+        if (r, c) in visited:
+            return 0
+        visited.add((r, c))
+        return dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)
+
+    # Start from the land portion
+    for r in range(ROWS):
+        for c in range(COLS):
+            if grid[r][c] == 1:
+                return dfs(r, c)
