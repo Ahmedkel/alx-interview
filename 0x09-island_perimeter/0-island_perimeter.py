7bf3e46620955937747807f195abdbc641a30051
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-06 16:12:21 +0100

    remaking the entire code (old code working)

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,20 +5,18 @@ main file
 
 
 def island_perimeter(grid):
-    """ Returns the perimeter of the island described by grid """
-    ROWS, COLS = len(grid), len(grid[0])
-    visited = set()
-
-    def dfs(r, c):
-        """ Returns the perimeter of the island described by grid """
-        if r < 0 or r == ROWS or c < 0 or c == COLS or grid[r][c] == 0:
-            return 1
-        if (r, c) in visited:
-            return 0
-        visited.add((r, c))
-        return dfs(r+1, c) + dfs(r-1, c) + dfs(r, c+1) + dfs(r, c-1)
-
-    for r in range(ROWS):
-        for c in range(COLS):
-            if grid[r][c] == 1:
-                return dfs(r, c)
+    """
+    returns the perimeter of
+    the island described in grid
+    """
+    row, column = len(grid), len(grid[0])
+    perimeter = 0
+    for i in range(row):
+        for j in range(column):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
