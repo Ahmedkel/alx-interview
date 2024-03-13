ed3c24d730a7c9e0b4087a80b17d008d87eec6a4
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-13 14:11:40 +0000

    fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,7 +8,6 @@ def isWinner(x, nums):
     ben_wins = 0
 
     def is_prime(n):
-        """ Determine if a number is prime. """
         if n < 2:
             return False
         for i in range(2, int(n**0.5) + 1):
@@ -18,27 +17,24 @@ def isWinner(x, nums):
 
     def remove_multiples(num, nums):
         """ Remove multiples of a number from a list. """
-        new_nums = []
-        for n in nums:
-            if n % num != 0:
-                new_nums.append(n)
-        return new_nums
+        return [n for n in nums if n % num != 0]
 
-    for _ in range(x):
-        maria_turn = True
-        while nums:
-            primes = [n for n in nums if is_prime(n)]
-            if not primes:
-                if maria_turn:
-                    ben_wins += 1
+    for n in nums:
+        if is_prime(n):
+            while True:
+                # Maria's turn
+                if n in nums:
+                    nums = remove_multiples(n, nums)
+                    n = next((x for x in nums if is_prime(x)), None)
+                    if n is None:
+                        maria_wins += 1
+                        break
+                # Ben's turn
                 else:
-                    maria_wins += 1
-                break
-            chosen_prime = max(
-                primes, key=lambda n: len(remove_multiples(n, nums)))
-            nums = remove_multiples(chosen_prime, nums)
-            maria_turn = not maria_turn
-
+                    n = next((x for x in nums if is_prime(x)), None)
+                    if n is None:
+                        ben_wins += 1
+                        break
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
